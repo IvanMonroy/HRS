efc183c3071fff3202eeee6dcb320bb67cdbcfc1
IvanMonroy <inter250296@hotmail.com>
2018-12-31 11:24:55 -0500

    subits

--- app/serializers/exit_serializer.rb
+++ app/serializers/exit_serializer.rb
@@ -1,3 +1,3 @@
 class ExitSerializer < ActiveModel::Serializer
-  attributes :entry_id, :date_departure, :hour_departure, :rate_id, total_time, :ammount_to_paid, :discount
+  attributes :entry_id, :date_departure, :hour_departure, :rate_id, :total_time, :ammount_to_paid, :discount
 end
