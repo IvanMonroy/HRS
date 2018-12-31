ae61a9f4d16cf2b6ff4bf9fd2d42860f7ec1ea71
IvanMonroy <inter250296@hotmail.com>
2018-12-31 11:22:55 -0500

    subit hkss

--- app/controllers/api/entry_controller.rb
+++ app/controllers/api/entry_controller.rb
@@ -1,6 +1,6 @@
 module Api
   class EntryController < ApplicationController
-    skip_before_filter :verify_authenticity_token
+    #skip_before_filter :verify_authenticity_token
     respond_to :json
     def all
     end
--- app/controllers/api/rate_controller.rb
+++ app/controllers/api/rate_controller.rb
@@ -1,6 +1,6 @@
 module Api
   class RateController < ApplicationController
-    skip_before_filter :verify_authenticity_token
+   # skip_before_filter :verify_authenticity_token
     respond_to :json
     def all
     end
