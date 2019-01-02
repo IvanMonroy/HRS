11c771b368fe5d95d253d02b90aa51624db92fc0
IvanMonroy <inter250296@hotmail.com>
2019-01-02 09:26:02 -0500

    subits

--- app/controllers/api/exits_controller.rb
+++ app/controllers/api/exits_controller.rb
@@ -36,6 +36,7 @@ end
              methods: %i[time_exit_format],
              include:{
                  entry:{
+                     only: %i[place is_parking date_arrival ],
                      methods: %i[time_entry_format],
                      include:{
                          vehicle:{
