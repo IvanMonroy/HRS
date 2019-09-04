579f2378c175f2d5506360faf74abd85bf4261f9
IvanMonroy <inter250296@hotmail.com>
2019-09-04 15:34:08 -0500

    Vehicle validations on create

--- app/models/concerns/vehicle_validation.rb
+++ app/models/concerns/vehicle_validation.rb
@@ -3,8 +3,22 @@ module VehicleValidation
   included do
     #validate :delete_rate, :on => :destroy
     before_destroy :delete_vehicle
-   # before_create :insert_vehicle
+   before_create :insert_vehicle, :length_plate, :length_year, :validate_year
   end
+
+  def lenght_plate
+    raise 'La placa deberá tener almenos 7 carácteres (Ej. ABC-123).' if self.plate.length < 7
+    raise 'Placa demasiado larga, porfavor escribirla en un formato correcto (Ej. ABC-123).' if self.plate.length > 7
+  end
+
+  def length_year
+    raise 'La placa deberá tener almenos 7 carácteres (Ej. ABC-123).' if self.year.length < 4
+  end
+
+  def validate_year
+    raise "El año insertado no es valido, inserte un año menor a #{Date.today.year + 1} " if self.year.to_i > (Date.today.year + 1)
+  end
+
   def delete_vehicle
     raise 'Error al eliminar el vehículo, hay entradas asociadas a este vehículo' if self.entry.exists?
   end
--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -2,8 +2,6 @@ class Vehicle < ApplicationRecord
   include VehicleValidation
   has_many :entry, :foreign_key => :plate, :primary_key => :plate
 
-  validates :plate, :length=> { :is =>7}
-  validates :brand, :length => { maximum: 200}
   validates :year, numericality: { greater_than_or_equal_to: 4}
   def name
     plate
