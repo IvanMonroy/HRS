d64652535cc1d2e4f6a54ec555a79aa0f57adbb4
IvanMonroy <inter250296@hotmail.com>
2019-09-04 15:40:58 -0500

    Vehicle validations on create

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -2,7 +2,6 @@ class Vehicle < ApplicationRecord
   include VehicleValidation
   has_many :entry, :foreign_key => :plate, :primary_key => :plate
 
-  validates :year, numericality: { greater_than_or_equal_to: 4}
   def name
     plate
   end
