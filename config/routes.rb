8e0e93580892b380dc6e06ce917c7fe381ec6c0f
IvanMonroy <inter250296@hotmail.com>
2019-11-12 09:49:15 -0500

    Create service show statistics

--- config/routes.rb
+++ config/routes.rb
@@ -24,14 +24,10 @@ Rails.application.routes.draw do
 
     end
     resources :exits, only: %i[index create update show destroy] do
-
-    end
-    resources :exits, only: %i[] do
       collection do
         get 'show_details/:id', action: :show_details
         get 'show_statistics', action: :show_statistics
       end
-
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
